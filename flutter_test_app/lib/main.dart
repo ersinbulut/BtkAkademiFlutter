e0282f97cd82149a85bf44dd3fcf2a38ef60e61d
Developer <Developer@DEV>
2025-03-28 11:20:55 +0300

    3.4_Container ve Center Widget'la

--- flutter_test_app/lib/main.dart
+++ flutter_test_app/lib/main.dart
@@ -18,7 +18,21 @@ class TestApp extends StatelessWidget{
           backgroundColor: Colors.blue,
           title: const Text("Test App"),
         ),
-        body: const Text("Merhaba Flutter"),
+        body: 
+          Container(
+            color: Colors.amber,
+            height: 200,
+            width: 200,
+            margin: EdgeInsets.symmetric(horizontal: 10,vertical: 10),
+            padding: EdgeInsets.all(8),
+            alignment: Alignment.center,
+            child: Container(
+            color: Colors.red,
+            alignment: Alignment.center,
+            width: 100,
+            height: 100,child: const Text("Hello world"),),
+            ),
+            
         floatingActionButton: FloatingActionButton(
           onPressed: () { 
           debugPrint("Butona tıklandı");
